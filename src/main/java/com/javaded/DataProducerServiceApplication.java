7f15da668d10a0f8832bcf4c5ffbebe724f4ba18
Andrei Soroka <131610832+JavaCoDED78@users.noreply.github.com>
2024-05-31 21:27:34 +0300

    init commit

---
+++ src/main/java/com/javaded/DataProducerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.javaded;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataProducerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataProducerServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/javaded/DataProducerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaded;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataProducerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
